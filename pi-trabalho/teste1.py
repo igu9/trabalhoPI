f041d1a8077e6d3c870a302ee17f7ec19c08466e
Igor Marques Reis <67602720+igu9@users.noreply.github.com>
2021-10-28 20:47:48 -0300

    Delete teste1.py

--- pi-trabalho/teste1.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-if __name__ == '__main__':
-    subprocess.run(["python.exe", "teste2.py", "oi", "meu", "grande amigo", "testando os bagui tudo aqui zim", "seloiro rapazote", "XD"])
